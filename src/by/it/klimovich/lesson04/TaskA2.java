32c49876275dde8576afee2c65e7195b68edbc61
selhozbis <selhozbis@gmail.com>
2018-11-01 00:42:33 +0300

    lesson 03 уровень а выполнен

--- src/by/it/klimovich/lesson04/TaskA2.java
+++ src/by/it/klimovich/lesson04/TaskA2.java
@@ -24,6 +24,23 @@ package by.it.klimovich.lesson04;
 
  */
 
+import java.util.Scanner;
+
 public class TaskA2 {
+    public static void main(String[] args) {
+        Scanner scanner=new(System.in);
+        int day=scanner.nextInt();
+        switch (day){
+            case 1: System.out.println("понедельник");break;
+            case 2:System.out.println("вторник");break;
+            case 3:System.out.println("среда");break;
+            case 4:System.out.println("четверг");break;
+            case 5:System.out.println("пятница");break;
+            case 6:System.out.println("суббота");break;
+            case 7:System.out.println("воскресенье");break;
+            default:System.out.println("такого дня недели не существует");break;
+
+        }
+    }
 
 }
